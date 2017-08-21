ba9e733104aeca71547d83a5c157f1066c3d08ba
Kat Solberg <katsolberg@gmail.com>
2017-08-21 16:56:18 -0700

    style about component

--- js/scenes/About/About.js
+++ js/scenes/About/About.js
@@ -5,14 +5,19 @@ import { Text, View, Image, FlatList } from 'react-native';
 import { styles } from './styles';
 
 const About = ({ data }) => (
-    <View>
-        <Image 
-            source={require('../../assets/images/r10_logo.png')}
-        />
-        <Text>R10 is a conference that focuses on just about any topic related to dev.</Text>
-        <Text style={styles.header}>Date & Venue</Text>
-        <Text>The R10 conference will take place on Tuesday, June 27, 2017 in Vancouver, BC.</Text>
-        <Text style={styles.header}>Code of Conduct</Text>
+    <View style={styles.container}>
+        <View style={styles.logoContainer}>
+            <Image 
+                source={require('../../assets/images/r10_logo.png')}
+                style={styles.logo}
+            />
+        </View>
+        <View style={styles.aboutInfo}>
+            <Text style={styles.aboutText}>R10 is a conference that focuses on just about any topic related to dev.</Text>
+            <Text style={styles.aboutHeader}>Date & Venue</Text>
+            <Text style={styles.aboutText}>The R10 conference will take place on Tuesday, June 27, 2017 in Vancouver, BC.</Text>
+            <Text style={styles.aboutHeader}>Code of Conduct</Text>
+        </View>
         <FlatList 
             data={data}
             renderItem={({item}) => 
--- js/scenes/About/styles.js
+++ js/scenes/About/styles.js
@@ -1,7 +1,31 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1
-    }
+        flex: 1,
+    },
+    logoContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30,
+        paddingBottom: 20,
+        borderBottomWidth: 1,
+        borderColor: '#e6e6e6',
+        marginRight: 10,
+        marginLeft: 10,
+    },
+    logo: {
+        height: 42,
+        width: 173,
+    },
+    aboutInfo: {
+        margin: 10,
+    },
+    aboutText: {
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    aboutHeader: {
+
+    },
 });
