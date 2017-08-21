b351684e43c933b6f7451c30bce284c08b5c1c25
Kat Solberg <katsolberg@gmail.com>
2017-08-21 16:30:12 -0700

    add Faves scene

---
+++ js/scenes/Faves/Faves.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+const Faves = () => (
+    <View>
+        <Text>
+            This is the Faves scene!
+        </Text>
+    </View>
+);
+
+Faves.propTypes = {
+
+};
+
+export default Faves;
---
+++ js/scenes/Faves/FavesContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Faves from './Faves';
+
+class FavesContainer extends Component {
+    
+    static PropTypes = {
+
+    };
+
+    render() {
+        return <Faves />;
+    }
+}
+
+export default FavesContainer;
