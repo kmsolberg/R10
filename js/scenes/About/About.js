b8a7635ce9bd0ac7116d40ffd8b65f09e215627f
Kat Solberg <katsolberg@gmail.com>
2017-08-21 16:09:27 -0700

    add loading to about component

--- js/scenes/About/About.js
+++ js/scenes/About/About.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import { styles } from './styles';
 
 const About = () => (
     <View>
-        <Text>
-            This is the about scene!
-        </Text>
+        <Image 
+            source={require('../../assets/images/r10_logo.png')}
+        />
+        <Text>R10 is a conference that focuses on just about any topic related to dev.</Text>
+        <Text style={styles.header}>Date & Venue</Text>
+        <Text>The R10 conference will take place on Tuesday, June 27, 2017 in Vancouver, BC.</Text>
+        <Text style={styles.header}>Code of Conduct</Text>
     </View>
 );
 
--- js/scenes/About/AboutContainer.js
+++ js/scenes/About/AboutContainer.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
 
 import About from './About';
 
 class AboutContainer extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+          data: [],
+          isLoading: true,
+        };
+      }
+    
+      componentDidMount() {
+        let endpoint = 'https://robot-data.firebaseio.com/robots.json';
+        fetch(endpoint)
+          // if fetch is successful, read our JSON out of the response
+          .then(response => response.json())
+          .then(data => {
+            this.setState({ data });
+          })
+          .catch(error => console.log(`Error fetching JSON: ${error}`));
+      }
+    
+      componentDidUpdate() {
+        if ( this.state.data && this.state.isLoading ) {
+          this.setState({ isLoading: false });
+        }
+      }
 
     static PropTypes = {
 
     };
 
     render() {
-        return <About />;
+        if (this.state.isLoading) {
+            return (
+             <ActivityIndicator animating={true} size="small" color="black" />
+            );
+        } else {
+            return (
+                <About />
+            )
+        }
     }
 }
-
-About.propTypes = {
-
-};
 
 export default AboutContainer;
